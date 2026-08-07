8a9f4293351cc61d82c4e96246a202b2ad7f6f6e
Shivom Sood <shivaysood10@gmail.com>
2026-08-07 11:43:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//const math = require("./math.js");
+
+//console.log(math.add(7,3));
+//console.log(math.subtract(7,3));
+
+//console.log("Hello World"); 
+
+import {add,subtract} from './math.js';
+
+console.log(add(9,2));
+console.log(subtract(9,2));
+
