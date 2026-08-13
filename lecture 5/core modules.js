8f2e9b4301166282297a110bec0448d8177a1911
Shivom Sood <shivaysood10@gmail.com>
2026-08-13 11:52:54 +0530

    Add fs module examples to lecture 5 (path, readFileSync, readFile)

--- lecture 5/core modules.js
+++ lecture 5/core modules.js
@@ -1,8 +1,27 @@
 const os=require('os');
+const path=require('path');
+const fs=require('fs');
 
 //console.log(os.platform())
 //console.log(os.arch())
 //console.log(os.cpus().length)
 //console.log(os.homedir())
-console.log(os.totalmem()/(1024*1024*1024) + "GB");
-console.log(os.freemem()/(1024*1024*1024) + "GB");cd ..
+//console.log(os.totalmem()/(1024*1024*1024) + "GB");
+//console.log(os.freemem()/(1024*1024*1024) + "GB");
+
+//const file="WEB DEV-III/lecture5/core_modules.js";
+//console.log(path.basename(file)); //file name
+//console.log(path.extname(file));  //.js
+//console.log(path.dirname(file));  // WEB DEV-III/lecture5
+
+const data =fs.readFileSync("sample.txt","utf-8"); 
+console.log(data);
+//console.log("B")
+//console.log("A")
+fs.readFile("sample.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    } 
+    })
